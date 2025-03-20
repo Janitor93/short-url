1fbd6e04614196cf7ba3bd3da958b96473942207
Janitor93 <ruslan.penkovskij@gmail.com>
2025-03-20 21:42:32 +0300

    feat(lib): generate specific redis key
    - redis key is a key of `service name` + `id`
    
    Closes #41

--- libs/common/src/redis/redis.service.ts
+++ libs/common/src/redis/redis.service.ts
@@ -1,19 +1,28 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
 
+import { PREFIX_TOKEN } from './constants';
+
 @Injectable()
 export class RedisService {
-  constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
+  constructor(
+    @Inject(PREFIX_TOKEN) private readonly service: string,
+    @Inject(CACHE_MANAGER) private readonly cache: Cache
+  ) {}
+
+  private getKey(key: string): string {
+    return `${this.service}/${key}`;
+  }
 
   async get<T>(key: string): Promise<T> {
-    return await this.cache.get(key);
+    return await this.cache.get(this.getKey(key));
   }
 
   async set(key: string, value: unknown) {
-    await this.cache.set(key, value);
+    await this.cache.set(this.getKey(key), value);
   }
 
   async del(key) {
-    await this.cache.del(key);
+    await this.cache.del(this.getKey(key));
   }
 }
