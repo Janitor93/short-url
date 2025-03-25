112b9f4c50d6223e0bfeb80f460520d30b132736
Janitor93 <ruslan.penkovskij@gmail.com>
2025-03-25 23:05:26 +0300

    feat(user): implement delete user method
    - added DELETE `/users/:id` method
    - added checking of existing users
    
    Closes #49

--- apps/user/src/user.service.ts
+++ apps/user/src/user.service.ts
@@ -12,12 +12,17 @@ export class UserService {
     private readonly passwordService: PasswordService,
   ) {}
 
-  public async checkEmail(email: string): Promise<void> {
+  private async checkEmail(email: string): Promise<void> {
     const result = await this.userRepository.isEmailExist(email);
     if (result) throw new ForbiddenException('This email is already registered!');
   }
 
-  public async createUser({ email, password }: CreateUserDto): Promise<User> {
+  private async checkUserById(id: string): Promise<void> {
+    const result = await this.userRepository.findById(id);
+    if (!result) throw new ForbiddenException('User doesn\'t exist.');
+  }
+
+  public async create({ email, password }: CreateUserDto): Promise<User> {
     await this.checkEmail(email);
     const result = await this.userRepository.save({
       email,
@@ -26,12 +31,19 @@ export class UserService {
     return result;
   }
 
-  public async getUserById(id: string): Promise<User> {
+  public async getById(id: string): Promise<User> {
+    await this.checkUserById(id);
     const user = await this.userRepository.findById(id);
     return user;
   }
 
-  public async updateUser(id: string, data: UpdateUserDto): Promise<User> {
+  public async update(id: string, data: UpdateUserDto): Promise<User> {
+    await this.checkUserById(id);
     return await this.userRepository.update(id, data);
+  }
+
+  public async delete(id: string): Promise<void> {
+    await this.checkUserById(id);
+    await this.userRepository.delete(id);
   }
 }
