87176782933636d2124efa5d42c133bbe836e69c
Janitor93 <ruslan.penkovskij@gmail.com>
2025-05-17 18:15:25 +0300

    feat(geoip): create geoip module
    - created GeoIpModule
    - country is stored in database
    
    Closes #85

--- apps/analytics/src/analytics.module.ts
+++ apps/analytics/src/analytics.module.ts
@@ -6,11 +6,13 @@ import { AnalyticsService } from './analytics.service';
 import { Analytics } from './analytics.entity';
 import { AnalyticsRepository } from './analytics.repository';
 import { appConfig } from './config';
+import { GeoIpModule } from './geoip/geoip.module';
 
 @Module({
   imports: [
     DatabaseModule.forRoot({ type: 'mongodb', entities: [Analytics] }),
     LoggerModule.registry(appConfig.logger.namespace),
+    GeoIpModule,
   ],
   controllers: [AnalyticsController],
   providers: [AnalyticsService, AnalyticsRepository],
